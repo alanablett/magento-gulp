91141ab976dbf85e5986deb0c6492c845f5e106c
Alan Ablett <alan.ablett1@googlemail.com>
2014-09-16 12:26:09 +0300

    Added some extra comments to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,9 @@ var destinationDir = './httpdocs/skin/frontend/themename/default';
 var destinationCssDirectory = destinationDir + '/css';
 var destinationJsDirectory = destinationDir + '/js';
 
+/**
+ * Pass our css through sass and auto prefixer
+ */
 gulp.task('css', function(){
     gulp.src(sourceMainCssFile)
         .pipe(sass()).on('error', gutil.log)
@@ -32,16 +35,27 @@ gulp.task('css', function(){
         .pipe(gulp.dest(destinationCssDirectory))
 });
 
+/**
+ * Pass our js through browserify
+ */
 gulp.task('js', function(){
     gulp.src(sourceMainJsFile)
     	.pipe(browserify()).on('error', gutil.log)
         .pipe(gulp.dest(destinationJsDirectory))
 });
 
+/**
+ * Set up our watcher tasks. This in turn will fire off the associated tasks
+ * when it sees a file has been changed.
+ */
 gulp.task('watch', function(){
     livereload.listen();
     gulp.watch(sourceCssDirectory + '/***/*.scss', ['css']).on('change', livereload.changed);
     gulp.watch(sourceJsDirectory +  '/***/*.js', ['js']).on('change', livereload.changed);
 });
 
+/**
+ * Set a default task. This allows us to run 'gulp' from the command line and it will
+ * assume we want to run the watch task. Otherwise we would have to run 'gulp watch'
+ */
 gulp.task('default', ['watch']);
